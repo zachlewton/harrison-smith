3235bb059d34ae7a79aff91a3b557d4fd91fe073
zachlewton <zach.g.lewton@gmail.com>
2021-04-19 16:39:00 -0400

    set up cms with fields

--- blog/blog-1.md
+++
@@ -1,6 +0,0 @@
----
-path: /blog/first-pos
-date: 2021-04-19T19:52:41.930Z
-title: blog 1
----
-bla bla bla
---
+++ gatsby-node.js
@@ -0,0 +1,56 @@
+const path = require("path")
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  // data.map((page) => {
+  // 	createPage({
+  // 		path: page.slug,
+  // 		component: path.resolve('./src/templates/Generic.js'),
+  // 		context: {
+  // 			title: page.title,
+  // 			description: page.description,
+  // 		},
+  // 	});
+  // });
+
+  // const mdPages = await graphql(`
+  // 	query MyQuery {
+  // 		allMarkdownRemark {
+  // 			edges {
+  // 				node {
+  // 					fields {
+  // 						slug
+  // 					}
+  // 				}
+  // 			}
+  // 		}
+  // 	}
+  // `);
+
+  // mdPages.data.allMarkdownRemark.edges.map(({ node }) => {
+  // 	createPage({
+  // 		path: node.fields.slug,
+  // 		component: path.resolve('./src/templates/Markdown.js'),
+  // 		context: {
+  // 			slug: node.fields.slug,
+  // 		},
+  // 	});
+  // });
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
