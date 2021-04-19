2ccd2c0d69723bc1fb23d16984149c0b6af3734f
zachlewton <zach.g.lewton@gmail.com>
2021-04-19 16:45:52 -0400

    move data to src folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `/data`,
+        path: `${__dirname}/src/data`,
         name: `data`,
       },
     },
